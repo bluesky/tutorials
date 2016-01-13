be3a6b3120e0b224d372f0a6b0dbfc9fcbbb842f
Daron Chabot <dchabot@bnl.gov>
2016-01-13 16:30:45 -0500

    CLN: tidy up

--- extensions/extensions/start_adsim.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen
-
-
-def load_jupyter_server_extension(nbapp):
-    nbapp.log.info('EPICS adsim - extension loaded')
-    Popen('procServ -q --name=adsim -i ^C^D^] -c /epics/iocs/adsim 2049 ./st.cmd', shell=True)
--- extensions/extensions/start_motorsim.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen
-
-
-def load_jupyter_server_extension(nbapp):
-    nbapp.log.info('EPICS motorsim - extension loaded')
-    Popen('procServ -q --name=motorsim -i ^C^D^] -c /epics/iocs/motorsim 2048 ./st.cmd', shell=True)
