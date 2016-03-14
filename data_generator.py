9eccaa0043348315322c83e5effb508745745ab2
Daron Chabot <dchabot@bnl.gov>
2016-03-14 11:44:10 -0400

    FIX: reflect api updates

--- data_generator.py
+++ data_generator.py
@@ -2,7 +2,7 @@ from bluesky.examples import motor, det
 # Note: importing from standard_config raises a qt/mpl-related error.
 from bluesky.global_state import gs
 from bluesky.register_mds import register_mds
-from bluesky.scans import AbsScan
+from bluesky.plans import AbsScanPlan
 
 
 register_mds(gs.RE)
@@ -12,5 +12,5 @@ gs.RE.md['config'] = {}
 gs.RE.md['beamline_id'] = 'demo'
 
 
-scan = AbsScan([det], motor, 1, 5, 5)
-gs.RE(scan, mood='optimisitc')
+scan = AbsScanPlan([det], motor, 1, 5, 5)
+gs.RE(scan, mood='optimistic')
