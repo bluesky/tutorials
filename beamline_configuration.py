b606de9805d7635bc136853b150588772f5a37a9
danielballan <daniel.b.allan@gmail.com>
2018-05-21 00:40:30 -0400

    Add tutorial connecting to pandas.

--- beamline_configuration.py
+++ beamline_configuration.py
@@ -8,9 +8,17 @@ os.environ['EPICS_CA_ADDR_LIST'] = '0.0.0.0'
 
 import matplotlib
 matplotlib.use('nbAgg')  # i.e. %matplotlib notebook
+
 import nslsii
 import ophyd
 from ophyd import Device, Component, EpicsSignal
+from ophyd.signal import EpicsSignalBase
+from ophyd.areadetector.filestore_mixins import resource_factory
+import uuid
+import os
+
+ophyd.set_cl('caproto')
+
 try:
     del RE
 except NameError:
@@ -18,16 +26,89 @@ except NameError:
 nslsii.configure_base(get_ipython().user_ns, 'temp')
 
 
+
 class Det(Device):
     det = Component(EpicsSignal, ':det', kind='hinted')
     exp = Component(EpicsSignal, ':exp', kind='config')
 
+# here there be 🐉🐉🐉🐉🐉🐉
+    
+class ArraySignal(EpicsSignalBase):
+    def __init__(self, read_pv, **kwargs):
+        super().__init__(read_pv, **kwargs)
+        cl = self.cl
+        base_pv, _ = read_pv.rsplit(':', maxsplit=1)
+        self._size_pv = cl.get_pv(
+            ':'.join((base_pv, 'ArraySize_RBV')))
+
+        self._last_ret = None
+        self._asset_docs_cache = []
+        
+    def trigger(self):
+        os.makedirs('/tmp/demo', exist_ok=True)
+        st = super().trigger()
+        ret = super().read()
+        val = ret[self.name]['value'].reshape(self._size_pv.get())
+        
+        resource, datum_factory = resource_factory(
+            spec='npy',
+            root='/tmp',
+            resource_path=f'demo/{uuid.uuid4()}.npy',
+            resource_kwargs={},
+            path_semantics='posix')
+        datum = datum_factory({})
+        self._asset_docs_cache.append(('resource', resource))
+        self._asset_docs_cache.append(('datum', datum))        
+        fpath = Path(resource['root']) / resource['resource_path']
+        np.save(fpath, val)                
+        
+        ret[self.name]['value'] = datum['datum_id']
+        self._last_ret = ret
+        return st
+
+    def describe(self):
+        ret = super().describe()
+        ret[self.name]['shape'] = [int(k)
+                                   for k in
+                                   self._size_pv.get()]
+        ret[self.name]['external'] = 'FILESTORE:'
+        return ret
+
+    def read(self):
+        if self._last_ret is None:
+            raise Exception('read before being triggered')
+        return self._last_ret
+            
+    def collect_asset_docs(self):
+        items = list(self._asset_docs_cache)
+        self._asset_docs_cache.clear()
+        for item in items:
+            yield item
+    
+class Dot(Device):
+    img = Component(ArraySignal, ':det')
+    exp = Component(EpicsSignal, ':exp', kind='config')
+    def collect_asset_docs(self):
+        yield from self.img.collect_asset_docs()
+
+    def trigger(self):
+        return self.img.trigger()
 
 ph = Det('jitter_read:ph', name='ph')
 edge = Det('jitter_read:edge', name='edge')
 slit = Det('jitter_read:slit', name='slit')
 
-motor_ph = EpicsSignal('jitter_read:ph:mtr', name='motor_ph')
-motor_edge = EpicsSignal('jitter_read:edge:mtr', name='motor_edge')
-motor_slit = EpicsSignal('jitter_read:slit:mtr', name='motor_slit')
+motor_ph = EpicsSignal('jitter_read:ph:mtr',
+                       name='motor_ph')
+motor_edge = EpicsSignal('jitter_read:edge:mtr',
+                         name='motor_edge')
+motor_slit = EpicsSignal('jitter_read:slit:mtr',
+                         name='motor_slit')
 
+dot = Dot('jitter_read:dot', name='dot')
+mtr_dotx = EpicsSignal('jitter_read:dot:mtrx',
+                       name='motor_dotx')
+mtr_doty = EpicsSignal('jitter_read:dot:mtrx',
+                       name='motor_doty')
+
+I = EpicsSignal('jitter_read:current', name='I')
