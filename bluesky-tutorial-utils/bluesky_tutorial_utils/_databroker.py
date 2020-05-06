3ed82c737f33331dcd7965fa8258a2b6dd539edc
Dan Allan <dallan@bnl.gov>
2020-05-06 19:50:22 -0400

    Make a utility package.

---
+++ bluesky-tutorial-utils/bluesky_tutorial_utils/_databroker.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import appdirs
+from event_model import RunRouter
+import intake
+from suitcase.msgpack import Serializer
+
+
+def setup_data_saving(RE):
+    """
+    Subscribe a suitcase Serializer to RE and return a corresponding Catalog.
+
+    The format happens to be msgback, but that should be treated as an
+    implementation detail subject to change.
+    """
+    directory = appdirs.user_data_dir("bluesky", "tutorial_utils")
+
+    def factory(name, start):
+        return [Serializer(directory)], []
+
+    rr = RunRouter([factory])
+    RE.subscribe(rr)
+    driver = intake.registry["bluesky-msgpack-catalog"]
+    return driver(str(Path(directory, "*.msgpack")))
