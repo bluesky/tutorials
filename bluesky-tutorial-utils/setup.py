5809b478b5dc60f82acfe9ef7ad96d1e2cbd0c9c
Dan Allan <dallan@bnl.gov>
2020-05-14 14:08:49 -0400

    Add and declare old npy handlers.

--- bluesky-tutorial-utils/setup.py
+++ bluesky-tutorial-utils/setup.py
@@ -5,4 +5,10 @@ setup(
     name="bluesky-tutorial-utils",
     packages=find_packages(),
     install_requires=["appdirs"],
+    entry_points={
+        "databroker.handlers": [
+            "npy = bluesky_tutorial_utils._old_handlers:NpyHandler",
+            "npy_FRAMEWISE = bluesky_tutorial_utils._old_handlers:NpyFrameWise",
+        ]
+    },
 )
