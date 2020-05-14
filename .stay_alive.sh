ffa93f14b9fab8dc3427ce1d7519295f57cb5238
Dan Allan <dallan@bnl.gov>
2020-05-14 14:56:12 -0400

    Abuse Travis.

---
+++ .stay_alive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Notebooks can take more than 10 minutes to render. Because they produce no
+# output to the stdout/stderr while rendering, Travis-CI gives up on us.
+# Reassure Travis by printing periodically.
+while true
+do
+    sleep 60
+    echo "This line was printed by .stay_alive.sh to keep Travis from giving up due to no output."
+done
