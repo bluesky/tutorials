7806d2b9d52af5e9d7f2dd7243c064c7284ac124
Thomas A Caswell <tcaswell@bnl.gov>
2020-05-14 15:30:15 -0400

    MNT: switch to put-completion on the mini beamline 'motors'

--- scripts/beamline_configuration.py
+++ scripts/beamline_configuration.py
@@ -163,18 +163,13 @@ ph = Det("mini:ph", name="ph")
 edge = Det("mini:edge", name="edge")
 slit = Det("mini:slit", name="slit")
 
-motor_ph = EpicsSignal('mini:ph:mtr',
-                       name='motor_ph')
-motor_edge = EpicsSignal('mini:edge:mtr',
-                         name='motor_edge')
-motor_slit = EpicsSignal('mini:slit:mtr',
-                         name='motor_slit')
+motor_ph = EpicsSignal("mini:ph:mtr", name="motor_ph", put_complete=True)
+motor_edge = EpicsSignal("mini:edge:mtr", name="motor_edge", put_complete=True)
+motor_slit = EpicsSignal("mini:slit:mtr", name="motor_slit", put_complete=True)
 
-spot = Spot('mini:dot', name='spot')
-mtr_spotx = EpicsSignal('mini:dot:mtrx',
-                        name='motor_spotx')
-mtr_spoty = EpicsSignal('mini:dot:mtry',
-                        name='motor_spoty')
+spot = Spot("mini:dot", name="spot")
+mtr_spotx = EpicsSignal("mini:dot:mtrx", name="motor_spotx", put_complete=True)
+mtr_spoty = EpicsSignal("mini:dot:mtry", name="motor_spoty", put_complete=True)
 
 I = EpicsSignal("mini:current", name="I")
 
