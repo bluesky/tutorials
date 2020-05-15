4f5ed08c717cb307e33eb13b1ba82a2aa2f2441f
Dan Allan <dallan@bnl.gov>
2020-05-15 14:15:17 -0400

    Fix syntax.

--- bluesky-tutorial-utils/bluesky_tutorial_utils/__init__.py
+++ bluesky-tutorial-utils/bluesky_tutorial_utils/__init__.py
@@ -2,6 +2,6 @@ from ._databroker import setup_data_saving  # noqa F401
 from . import fetch  # noqa F401
 from ._example_data import (
     generate_example_data,
-    save_example_data
+    save_example_data,
     get_example_catalog
 )
