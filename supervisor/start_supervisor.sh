eaa71b32950bd28a31655a8d10d387271bbf9348
Dan Allan <dallan@bnl.gov>
2021-07-02 12:07:13 -0400

    Let start_supervisor accepts status/stop/restart/etc

--- supervisor/start_supervisor.sh
+++ supervisor/start_supervisor.sh
@@ -1,10 +1,6 @@
-# Start supervisord if is not already running.
-
-# Once jupyter-repo2docker supports a running a script at container
-# start time (i.e. ENTRYPOINT) then we can run this that way and
-# no action will be required by the user.
-
+# Locate the directory that contains this script.
 DIR=$( cd "$(dirname "$0")" ; pwd -P )
-
+# Start supervisord if is not already running.
 [ ! -e /tmp/supervisor.sock ] && supervisord -c $DIR/supervisord.conf
-supervisorctl -c $DIR/supervisord.conf status
+# The processes managed by supervisord are being auto-started.
+supervisorctl -c $DIR/supervisord.conf "$@"
