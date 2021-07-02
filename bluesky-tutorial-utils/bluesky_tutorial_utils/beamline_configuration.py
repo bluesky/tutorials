77934776beb17c89ca23636ea472d9ddacfa757d
Dan Allan <dallan@bnl.gov>
2021-07-02 13:21:39 -0400

    Use BestEffortCallback but only for tables.

--- bluesky-tutorial-utils/bluesky_tutorial_utils/beamline_configuration.py
+++ bluesky-tutorial-utils/bluesky_tutorial_utils/beamline_configuration.py
@@ -45,7 +45,7 @@ from bluesky.magics import BlueskyMagics
 
 get_ipython().register_magics(BlueskyMagics)
 
-# Set up the BestEffortCallback.
+# Set up plots with bluesky_widgets.
 from bluesky_widgets.models.auto_plot_builders import AutoLines
 from bluesky_widgets.utils.streaming import stream_documents_into_runs
 from bluesky_widgets.jupyter.figures import JupyterFigures
@@ -53,6 +53,14 @@ from bluesky_widgets.jupyter.figures import JupyterFigures
 auto_plot_model = AutoLines(max_runs=10)
 RE.subscribe(stream_documents_into_runs(auto_plot_model.add_run))
 auto_plot_view = JupyterFigures(auto_plot_model.figures)
+
+# Use BestEffortCallback just for the table
+# TODO: Retire our use of BestEffortCallback, using a table from
+# bluesky_widgets once one is available.
+from bluesky.callbacks.best_effort import BestEffortCallback
+bec = BestEffortCallback()
+bec.disable_plots()
+RE.subscribe(bec)
 
 # Import matplotlib and put it in interactive mode.
 import matplotlib.pyplot as plt
