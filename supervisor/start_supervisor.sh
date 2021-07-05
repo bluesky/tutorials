1b3c8a50ab7188219fc76fab8eef97bbdd3f6c9f
Dan Allan <dallan@bnl.gov>
2021-07-05 09:21:05 -0400

    Debug CI failures. Try adding a sleep on first supervisor startup.

--- supervisor/start_supervisor.sh
+++ supervisor/start_supervisor.sh
@@ -4,7 +4,11 @@
 DIR=$( cd "$(dirname "$0")" ; pwd -P )
 
 # Start supervisord if is not already running.
-[ ! -e /tmp/supervisor.sock ] && supervisord -c $DIR/supervisord.conf
+# (Include a sleep to reduce chances of a race between IOC startup
+# and subsequent code that relies on IOCs being started.
+# This race is unlikely to matter for interactive use but can come up
+# in the context of CI when cells are executed top-to-bottom.)
+[ ! -e /tmp/supervisor.sock ] && supervisord -c $DIR/supervisord.conf && sleep 2
 
 # The processes managed by supervisord are being auto-started.
 if [ $# -gt 0 ]; then
