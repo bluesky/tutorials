11d8a64516f9158fe6cba96fdff09dcb0061bc68
Andi Barbour <abarbour@bnl.gov>
2023-04-21 10:44:16 -0400

    accidently committed file with wrong file name so removed it

--- gm_user/user_startup_afternoon-Copy1.py
+++
@@ -1,8 +0,0 @@
-
-
-### Setup to make notebook behave like a single bluesky terminal session
-RE.md["purpose"] = 'setup'
-
-
-### Setup up current session to be "later in the day"
-RE(mv(motor, 10, temperature, 30))
